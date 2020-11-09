aff5e010b3b647bd257ecc40be34de56d8df62cc
JQB <jordanqbehn@gmail.com>
2020-11-09 12:24:56 -0600

    added render five day forecast function

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ var mom = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(mom);
 //on click of button run api call
 $("#search").click(function() {
-    renderToday();
-    renderFiveDay();
+    let cityInput = $("#cityInput").val();
+
+    renderToday(cityInput);
+    renderFiveDay(cityInput);
 })
 
-function renderToday() {
-    let cityInput = $("#cityInput").val();
-    const apikey = "87f0c9d5a9813668cae2ebe11bcb2972";
-    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + apikey;
+function renderToday(cityInput) {
+    //apikey = "87f0c9d5a9813668cae2ebe11bcb2972";
+    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=87f0c9d5a9813668cae2ebe11bcb2972";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -31,19 +32,44 @@ function renderToday() {
         //get lat and long for uv index, call uv index
         let lat = response.city.coord.lat;
         let long = response.city.coord.lon;
-        console.log(lat);
-        console.log(long);
-        let uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + apikey;
+
+        let uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=87f0c9d5a9813668cae2ebe11bcb2972";
         $.ajax({
             url: uvURL,
             method: "GET"
         }).then(function(UVresponse) {
-            console.log(UVresponse.value);
+            // console.log(UVresponse.value);
             $("#uv-index").text("UV index: " + UVresponse.value);
         })
     })
 }
 
-function renderFiveDay() {
+function renderFiveDay(cityInput) {
+    $(".days").html("")
+    let currentDate = moment().date();
+    for (i = 1; i < 6; i++) {
+        //make div for each day
+        let dayDiv = $("<day-" + i + " id=day-" + i + ">");
+        $(".days").append(dayDiv);
+        //increment date
+        dayInc = moment().date(currentDate + i).format("MM/DD");
+        //query for weather info
+        //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
 
+        let fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityInput + "&cnt=6&appid=87f0c9d5a9813668cae2ebe11bcb2972";
+        console.log(fiveDayURL);
+        $.ajax({
+                url: fiveDayURL,
+                method: "GET"
+            }).then(function(fiveDayresponse) {
+                // console.log(fiveDayresponse);
+            })
+            //add date, icon, temp, humidity
+        $("#day-" + i).append("<h2>" + dayInc + "</h2");
+    }
 }
+// let currentDate = moment().date();
+// console.log(moment().date());
+// console.log(currentDate + 1);
+// currentDate++;
+// console.log(moment().date(currentDate).format("MM/DD"))
