9c0776fa4c0f7a8a6a523cbcd8eaeecafa0a98ea
JQB <jordanqbehn@gmail.com>
2020-11-09 10:19:11 -0600

    added render today weather function

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 //get current moment
-var mom = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var mom = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(mom);
 //on click of button run api call
 $("#search").click(function() {
+    renderToday();
+    renderFiveDay();
+})
+
+function renderToday() {
     let cityInput = $("#cityInput").val();
-    let apikey = "87f0c9d5a9813668cae2ebe11bcb2972";
+    const apikey = "87f0c9d5a9813668cae2ebe11bcb2972";
     let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + apikey;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+        //store important info about today's weather
         console.log(response)
         let city = response.city.name;
         let temp = response.list[0].main.temp;
         let humidity = response.list[0].main.humidity;
         let wind = response.list[0].wind.speed;
+        let weatherIcon = response.list[0].weather[0].icon;
+        console.log(weatherIcon);
+        //display weather info
+        $('#city').text(city);
+        $('#temp').text("Temperature: " + temp);
+        $('#humidity').text("Humidity: " + humidity);
+        $('#wind').text("Windspeed: " + wind);
         //get lat and long for uv index, call uv index
         let lat = response.city.coord.lat;
         let long = response.city.coord.lon;
@@ -29,4 +42,8 @@ $("#search").click(function() {
             $("#uv-index").text("UV index: " + UVresponse.value);
         })
     })
-})
+}
+
+function renderFiveDay() {
+
+}
